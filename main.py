e786a39fb5db956fbdb7000c8d8cd32e2078e13d
Voltas-Frog <lancer.evolution.9x@gmail.com>
2026-05-22 19:53:43 -0500

    Initialized file

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+VERSION = pygame.version.ver
+
+def main():
+    print(f"Starting Asteroids with pygame version: {VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
