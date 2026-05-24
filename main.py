cde4a735d08f33552550913347f681097187bd84
Voltas-Frog <lancer.evolution.9x@gmail.com>
2026-05-24 17:40:01 -0500

    I: added Asteroid fields, new files asteroid.py and asteroidfield.py. updated files constants.py and main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 VERSION = pygame.version.ver
 
@@ -22,10 +25,14 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     PLAYER = Player(x, y)
+    ASTEROIDS = AsteroidField()
     
     while True:
         log_state()
